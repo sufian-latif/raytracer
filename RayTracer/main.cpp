ff06abb42168272235ae499ef50dbc21b05271cf
Sufian Latif <sufianlatif@Sufians-Macbook-Pro.local>
2014-01-26 21:37:39 +0600

    Initial Commit

---
+++ RayTracer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RayTracer
+//
+//  Created by Sufian Latif on 1/26/14.
+//  Copyright (c) 2014 Sufian Latif. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
